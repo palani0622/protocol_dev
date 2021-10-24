3994524d54753613a96533040cf29dae9953ce51
palani <palaniyaappan@gmail.com>
2021-10-24 23:17:00 +0530

    i have changed to 1.3 to 1.4

---
+++ course/palani1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+int p,n;
+float r,si;
+printf("Entre the values of r:     ");
+printf("Entre the values of n:     ");
+printf("Entre the values of p:     ");
+scanf("%d\n",&p);
+scanf("%d\n",&n);
+scanf("%f\n",&r);
+si= p*n*r/100;
+printf("%f\n",si);
+return 0;
+}

b70554b91c8549acc5b7eb46bac8aa55fc3eec9f
palani <palaniyaappan@gmail.com>
2021-10-27 08:16:40 +0530

    moved to dir

---
+++ C_programming/Chapter1/palani3.c
@@ -0,0 +1,12 @@
+/* Just for fun. Author: palani */
+#include<stdio.h>
+int main()
+{
+int num;
+printf("Entre the number:  ");
+scanf("%d",&num);
+printf("just enerted the number is:   %d\n",num);
+return 0;
+}
+
+

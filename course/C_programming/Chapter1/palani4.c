9b31740af1c651eab9cadbd986ef5d3f745dbbf7
palani <palaniyaappan@gmail.com>
2021-10-27 08:20:59 +0530

    moved to dir

---
+++ course/C_programming/Chapter1/palani4.c
@@ -0,0 +1,11 @@
+/* Add two numbers */
+#include <stdio.h>
+int main( )
+{
+int a, b, c ;
+printf ( "Enter values of a,b:   " ) ;
+scanf ("%d %d",&a,&b);
+c=a+b;
+printf("%d\n",c);
+return 0;
+}
---
+++ course/C_programming/Chapter1/troublshoot.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+
+	float a,b, c;
+	printf("please enter the a value: ");
+	scanf("%f",&a);
+        printf("please enter the b value: ");
+	scanf("%f",&b);
+	c=a/b;
+	printf("Result= %f\n",c);
+	
+}
